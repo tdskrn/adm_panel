859c68fced9836839ba5ec48d6b4982ca0a1a6a9
Vinicius S. Rocha <viniciusbbbjj@gmail.com>
2023-03-14 20:26:38 -0300

    elevated button finish

--- lib/views/screens/side_bar_screen/upload_banners_screen.dart
+++ lib/views/screens/side_bar_screen/upload_banners_screen.dart
@@ -1,3 +1,4 @@
+import 'package:adm_panel/utils/colors/colors_marques.dart';
 import 'package:flutter/material.dart';
 
 class UploadBannersScreen extends StatelessWidget {
@@ -26,20 +27,52 @@ class UploadBannersScreen extends StatelessWidget {
             children: [
               Padding(
                 padding: const EdgeInsets.all(14.0),
-                child: Container(
-                  height: 140,
-                  width: 140,
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade500,
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text("Banners"),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 140,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade500,
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text("Banners"),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          foregroundColor: ColorsMarques.blueMarques),
+                      onPressed: () {},
+                      child: Text(
+                        "Upload Image",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: ColorsMarques.blueMarques,
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Save",
+                  style: TextStyle(
+                    color: Colors.white,
                   ),
                 ),
               ),
-              ElevatedButton(onPressed: () {}, child: Text("Upload Banner"))
             ],
           ),
         ],
