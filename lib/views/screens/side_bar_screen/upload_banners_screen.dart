b3ea88546bf80df32e14a757f34bb95f38201343
Vinicius S. Rocha <viniciusbbbjj@gmail.com>
2023-03-14 20:06:26 -0300

    deploy firebase funcionando

--- lib/views/screens/side_bar_screen/upload_banners_screen.dart
+++ lib/views/screens/side_bar_screen/upload_banners_screen.dart
@@ -6,16 +6,43 @@ class UploadBannersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          "Upload Banners",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 36,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              "Banners",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 36,
+              ),
+            ),
           ),
-        ),
+          Divider(
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Container(
+                  height: 140,
+                  width: 140,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade500,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text("Banners"),
+                  ),
+                ),
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("Upload Banner"))
+            ],
+          ),
+        ],
       ),
     );
   }
